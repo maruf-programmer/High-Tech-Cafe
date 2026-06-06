37a03928c44819b9b9560a8baa08a5b23fd09f80
texnoflow-droid <texnoflow@gmail.com>
2026-06-06 11:35:58 +0500

    UI/UX Overhaul: Premium visuals for Home and Staff Dashboard

--- cafe_project/settings.py
+++ cafe_project/settings.py
@@ -35,7 +35,7 @@ ROOT_URLCONF = 'cafe_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
